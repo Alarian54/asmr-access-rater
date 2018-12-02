acda75092d90af5d4f6be8053984d4d4cbdadf2d
Angus Shaw <shawa950@gmail.com>
2018-12-01 18:00:35 -0800

    Update Place.java

--- src/model/Place.java
+++ src/model/Place.java
@@ -5,12 +5,17 @@ import java.util.List;
 import java.util.Objects;
 
 public class Place {
-
     private String name;
+    private String type;
+    private int ticks;
+    private int maxTicks;
     private List<Review> reviews;
 
-    public Place(String name) {
+    public Place(String name, String type, int ticks, int maxTicks) {
         this.name = name;
+        this.type = type;
+        this.ticks = ticks;
+        this.maxTicks = maxTicks;
         this.reviews = new ArrayList<>();
     }
 
@@ -18,15 +23,57 @@ public class Place {
         return name;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public int getTicks() {
+        return ticks;
+    }
+
+    public int getMaxTicks() {
+        return maxTicks;
+    }
+
+    public void setTicks(int i) {
+        ticks = i;
+    }
+
+    public void setMaxTicks(int i) {
+        maxTicks = i;
+    }
+
+    public void addTick() {
+        ticks += 1;
+    }
+
+    public void addMaxTick() {
+        maxTicks += 1;
+    }
+
+    public void addReview(Review review) {
+        reviews.add(review);
+        ticks = 0;
+        for (Review r : reviews) {
+            ticks+=review.getStars();
+        }
+        ticks = ticks/reviews.size();
+    }
+
     public List<String> printArray() {
         List<String> printArray = new ArrayList<>();
         printArray.add(name);
-        for (Review review: reviews){
-            printArray.add(" - " + review.string());
+        if (maxTicks==0) {
+            printArray.add(" - Not scored yet");
+        } else {
+            printArray.add(" - Score: " + ticks + "/" + maxTicks);
         }
-        if (reviews.size()==0) {
-            printArray.add(" - No reviews yet");
-        }
+//        for (Review review: reviews){
+//            printArray.add(" - " + review.string());
+//        }
+//        if (reviews.size()==0) {
+//            printArray.add(" - No reviews yet");
+//        }
         return printArray;
     }
 
